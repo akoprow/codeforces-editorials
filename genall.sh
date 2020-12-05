070902a00b11aca191fd32d0ce83db7561af3d58
Adam Koprowski <Adam.Koprowski@gmail.com>
2020-12-05 22:58:17 +0000

    Adding R-686

--- genall.sh
+++ genall.sh
@@ -1,8 +1,10 @@
 ##!/usr/bin/env bash
 
 ./gen.py 1431 KH-5   "Kotlin Heroes 5"
-./gen.py 1454 R-686  "Round 686 (Div III)"
+
 ./gen.py 1455 Edu-99 "Edu 99"
-./gen.py 1456 R-687  "Round 687 (Div I)"
-./gen.py 1457 R-687  "Round 687 (Div II)"
+
 ./gen.py 1438 R-682 "Round 682 (Div II)"
+./gen.py 1454 R-686 "Round 686 (Div III)"
+./gen.py 1456 R-687 "Round 687 (Div I)"
+./gen.py 1457 R-687 "Round 687 (Div II)"
